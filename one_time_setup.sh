50e26d2df08bbb35d650cc564e038f9aab67d2f3
abhinavg97 <gabhinav001@gmail.com>
2024-02-27 19:52:45 -0500

    add log messaged

--- one_time_setup.sh
+++ one_time_setup.sh
@@ -8,5 +8,10 @@ fi
 
 bash setup_docker.sh
 
+echo "setting up docker completed"
+
 # the first argument is the bridge prefix like so - 192.167.1.1 or 192.168.1.1
 bash setup_bridge_firecracker.sh ${BRIDGE_PREFIX}
+
+
+echo "setting up bridge and downloading firecracker and firectl binaries completed"
