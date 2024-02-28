65ae109891aba8e3238da7e9ae22c3bf042c73d6
abhinavg97 <gabhinav001@gmail.com>
2024-02-27 19:28:12 -0500

    change to give an argument to setup script

--- one_time_setup.sh
+++ one_time_setup.sh
@@ -1,4 +1,5 @@
+BRIDGE_PREFIX=$1
 bash setup_docker.sh
 
-# the first argument is the bridge mask like so - 192.167.1.1 or 192.168.1.1
-bash setup_bridge_firecracker.sh "192.167"
+# the first argument is the bridge prefix like so - 192.167.1.1 or 192.168.1.1
+bash setup_bridge_firecracker.sh ${BRIDGE_PREFIX}
