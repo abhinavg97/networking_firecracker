162a91a6ff3ac41a26a8a7bbe0d07e3ec183dc22
abhinavg97 <gabhinav001@gmail.com>
2024-03-02 16:54:12 -0500

    remove python dependency

--- workloads/ping_all.sh
+++ workloads/ping_all.sh
@@ -14,23 +14,3 @@ do
 done
 
 sleep 120
-
-# Initialize variables
-total=0
-count=0
-
-
-for (( VM_INDEX=1; VM_INDEX<=$NUM_VMS; VM_INDEX++));
-do
-  file=ping_${VM_INDEX}
-  value=$(cat "$file")
-  total=$(echo "$total + $value" | bc)
-  ((count++))
-
-done
-
-average=$(echo "scale=3; $total / $count" | bc)
-
-echo $average
-
-# python3 calc_ping_metrics.py $NUM_VMS
