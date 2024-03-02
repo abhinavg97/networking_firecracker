f175dbde930d32a9b724cbd138cc28cb756e0682
abhinavg97 <gabhinav001@gmail.com>
2024-03-02 16:40:55 -0500

    remove python dependency

--- ping_exp.sh
+++ ping_exp.sh
@@ -30,7 +30,8 @@ do
         for (( SRC_VM_INDEX=1; SRC_VM_INDEX<=$SOURCE_VMS; SRC_VM_INDEX++ ));
         do
             SRC_VM_IP="$(printf '%s.1.%s' ${SOURCE_BRIDGE_PREFIX} $(((2 * SRC_VM_INDEX + 1) )))"
-            scp -i rootfs.id_rsa ~/${REPO_NAME}/workloads/ping_all.sh root@$SRC_VM_IP:/ping_all.sh
+            cat ~/${REPO_NAME}/workloads/ping_all.sh | ssh -i rootfs.id_rsa root@$SRC_VM_IP "cat > ping_all.sh"
+            # scp -i rootfs.id_rsa ~/${REPO_NAME}/workloads/ping_all.sh root@$SRC_VM_IP:~/ping_all.sh
             ssh -i rootfs.id_rsa root@$SRC_VM_IP bash ping_all.sh $TARGET_VMS $TARGET_BRIDGE_PREFIX &
             pids+=($!)
         done
@@ -43,7 +44,8 @@ do
         for (( SRC_VM_INDEX=1; SRC_VM_INDEX<=$SOURCE_VMS; SRC_VM_INDEX++ ));
         do
             SRC_VM_IP="$(printf '%s.1.%s' ${SOURCE_BRIDGE_PREFIX} $(((2 * SRC_VM_INDEX + 1) )))"
-            scp -i rootfs.id_rsa ~/${REPO_NAME}/workloads/collect_ping_metrics.py root@$SRC_VM_IP:/collect_ping_metrics.py
+            cat ~/${REPO_NAME}/workloads/collect_ping_metrics.py | ssh -i rootfs.id_rsa root@$SRC_VM_IP "cat > collect_ping_metrics.py"
+            # scp -i rootfs.id_rsa ~/${REPO_NAME}/workloads/collect_ping_metrics.py root@$SRC_VM_IP:~/collect_ping_metrics.py
             ssh -i rootfs.id_rsa root@$SRC_VM_IP python3 collect_ping_metrics.py $TARGET_VMS >> "ping_${SOURCE_VMS}x${TARGET_VMS}_${SRC_VM_INDEX}"
             sleep 5
         done
--- workloads/ping_all.sh
+++ workloads/ping_all.sh
@@ -9,10 +9,28 @@ fi
 
 for (( VM_INDEX=1; VM_INDEX<=$NUM_VMS; VM_INDEX++));
 do
-        TUN_IP="$(printf '%s.1.%s' ${BRIDGE_PREFIX} $(((2 * VM_INDEX + 1) )))"
-        ping -c 100 $TUN_IP | grep rtt > ping_${VM_INDEX} &
+  TUN_IP="$(printf '%s.1.%s' ${BRIDGE_PREFIX} $(((2 * VM_INDEX + 1) )))"
+  ping -c 100 $TUN_IP | grep rtt | awk -F'[/ ]+' '{print $8}' > ping_${VM_INDEX} &
 done
 
 sleep 120
 
-python3 calc_ping_metrics.py $NUM_VMS
+# Initialize variables
+total=0
+count=0
+
+
+for (( VM_INDEX=1; VM_INDEX<=$NUM_VMS; VM_INDEX++));
+do
+  file=ping_${VM_INDEX}
+  value=$(cat "$file")
+  total=$(echo "$total + $value" | bc)
+  ((count++))
+
+done
+
+average=$(echo "scale=3; $total / $count" | bc)
+
+echo $average
+
+# python3 calc_ping_metrics.py $NUM_VMS
