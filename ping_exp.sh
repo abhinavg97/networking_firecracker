e2f611e139f7e1ebf16dfc12c3679e82e343ed6e
abhinavg97 <gabhinav001@gmail.com>
2024-03-02 14:14:12 -0500

    parse repo name

--- ping_exp.sh
+++ ping_exp.sh
@@ -4,6 +4,8 @@ TARGET_NODE="node$3"
 SOURCE_BRIDGE_PREFIX=$4
 TARGET_BRIDGE_PREFIX=$5
 
+REPO_NAME=$(basename `git rev-parse --show-toplevel`)
+
 if [ "$#" -ne 5 ]
 then
   echo "run like: ping_nodex.sh [TOTAL_SOURCE_VMS]10 [TOTAL_TARGET_VMS]10 [TARGET_NODE]1 [SOURCE_BRIDGE_PREFIX]192.167 [TARGET_BRIDGE_PREFIX]192.168"
@@ -28,7 +30,7 @@ do
         for (( SRC_VM_INDEX=1; SRC_VM_INDEX<=$SOURCE_VMS; SRC_VM_INDEX++ ));
         do
             SRC_VM_IP="$(printf '%s.1.%s' ${SOURCE_BRIDGE_PREFIX} $(((2 * SRC_VM_INDEX + 1) )))"
-            scp -i rootfs.id_rsa ~/firecracker_networking/workloads/ping_all.sh root@$SRC_VM_IP:/ping_all.sh
+            scp -i rootfs.id_rsa ~/${REPO_NAME}/workloads/ping_all.sh root@$SRC_VM_IP:/ping_all.sh
             ssh -i rootfs.id_rsa root@$SRC_VM_IP bash ping_all.sh $TARGET_VMS $TARGET_BRIDGE_PREFIX &
             pids+=($!)
         done
@@ -41,7 +43,7 @@ do
         for (( SRC_VM_INDEX=1; SRC_VM_INDEX<=$SOURCE_VMS; SRC_VM_INDEX++ ));
         do
             SRC_VM_IP="$(printf '%s.1.%s' ${SOURCE_BRIDGE_PREFIX} $(((2 * SRC_VM_INDEX + 1) )))"
-            scp -i rootfs.id_rsa ~/firecracker_networking/workloads/collect_ping_metrics.py root@$SRC_VM_IP:/collect_ping_metrics.py
+            scp -i rootfs.id_rsa ~/${REPO_NAME}/workloads/collect_ping_metrics.py root@$SRC_VM_IP:/collect_ping_metrics.py
             ssh -i rootfs.id_rsa root@$SRC_VM_IP python3 collect_ping_metrics.py $TARGET_VMS >> "ping_${SOURCE_VMS}x${TARGET_VMS}_${SRC_VM_INDEX}"
             sleep 5
         done
