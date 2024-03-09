628c3abc3f7fd47d0b8641153961e8cc7bfb7b44
abhinavg97 <gabhinav001@gmail.com>
2024-03-09 14:43:36 -0500

    remove uneccesary temp command

--- server/parallel_start_many.sh
+++ server/parallel_start_many.sh
@@ -38,14 +38,3 @@ do
 
         DUMMY=10
 done
-
-
-
-firectl \
-        --firecracker-binary=/usr/local/bin/firecracker \
-        --kernel=$HOME/networking_firecracker/rootfs.vmlinux \
-        --kernel-opts="init=/sbin/boottime_init panic=1 pci=off nomodules reboot=k tsc=reliable quiet i8042.nokbd i8042.noaux 8250.nr_uarts=0 ipv6.disable=1 ip=${TUN_IP}::${BRIDGE_IP}:::eth0:off:${DNS0_IP}" \
-        --root-drive=$HOME/networking_firecracker/rootfs.ext4 \
-        --log-level=Error \
-        -l=error.log \
-        --tap-device=$TAP_DEV/$TAP_MAC
