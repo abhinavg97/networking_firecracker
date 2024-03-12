c40de8f9cfa48da0e4a8ec73a258d45bbb1527dc
abhinavg97 <gabhinav001@gmail.com>
2024-03-12 19:22:18 -0400

    correct while loop var enable vm networking

--- server/enable_vm_networking.sh
+++ server/enable_vm_networking.sh
@@ -8,20 +8,20 @@ NUM_VMS=$1
 BRIDGE_PREFIX=$2
 REPO_NAME=$(basename `git rev-parse --show-toplevel`)
 
-for (( VM_INDEX=1; VM_INDEX<=${NUM_VMS}; NUM_VMS++ ));
+for (( VM_INDEX=1; VM_INDEX<=${NUM_VMS}; VM_INDEX++ ));
 do
 
     VM_IP="$(printf '%s.1.%s' ${BRIDGE_PREFIX} $(((2 * VM_INDEX + 1) )))"
 
     while true; do
         ssh -i $HOME/$REPO_NAME/rootfs.id_rsa root@$VM_IP "echo nameserver 8.8.8.8 >> /etc/resolv.conf"
-        ssh -i $HOME/$REPO_NAME/rootfs.id_rsa root@$VM_IP "echo nameserver 8.8.4.4 >> /etc/resolv.conf"
-
         if [ $? -eq 0 ]; then
+            echo "root@$VM_IP done"
             break
         else
             echo "Retrying to ssh root@$VM_IP ..."
             sleep 1
         fi
     done
+    ssh -i $HOME/$REPO_NAME/rootfs.id_rsa root@$VM_IP "echo nameserver 8.8.4.4 >> /etc/resolv.conf"
 done
