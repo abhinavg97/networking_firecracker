5fbdf44f1319e197f3b7aa214f15d935e165291c
abhinavg97 <gabhinav001@gmail.com>
2024-03-29 12:04:08 -0400

    correct bare metal to bare metal iperf testing

--- iperf_b1_b2.sh
+++ iperf_b1_b2.sh
@@ -1,6 +1,6 @@
 if [ "$#" -ne 5 ]
 then
-  echo "run like: iperf_exp.sh [MIN_CONST_VMS]1 [TOTAL_CONST_VMS]10 [TARGET_NODE_IP]10.10.1.1 [SOURCE_BRIDGE_PREFIX]192.168 [TARGET_BRIDGE_PREFIX]192.167"
+  echo "run like: iperf_exp.sh [MIN_CONST_PORTS]1 [TOTAL_CONST_PORTS]10 [TARGET_NODE_IP]10.10.1.1 [SOURCE_BRIDGE_PREFIX]192.168 [TARGET_BRIDGE_PREFIX]192.167"
   exit 1
 fi
 
@@ -26,7 +26,7 @@ do
     for (( PORT_INDEX=1; PORT_INDEX<=$CONST_PORTS; PORT_INDEX++ ));
     do
 		PORT=$((PORT_INDEX + PIVOT_PORT))
-        ## start iperf3 client in the source vm
+        ## start iperf3 client in the source machine
         iperf3 -c $TARGET_NODE -t 300 -f g -i 0 -p $PORT > iperf_${PORT} &
         pids+=($!)
     done
@@ -46,9 +46,12 @@ do
         total=$(echo "$total + $value" | bc)
     done
 
-    average=$(bc <<< "scale=5; $total / $CONST_VMS")
+    average=$(bc <<< "scale=5; $total / $CONST_PORTS")
 
-    echo $average > iperf_${CONST_VMS}
+    echo $average > iperf_${CONST_PORTS}
+
+	ssh ag4786@$TARGET_NODE "killall iperf3"
+	killall iperf3
 
     sleep 5
 done
